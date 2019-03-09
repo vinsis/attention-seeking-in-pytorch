453978cfcb0b5f4c66807a744024596c504b34d5
vinsis <6437295+vinsis@users.noreply.github.com>
2019-03-09 11:12:19 +0800

    implemented attention the right way only now -____-

--- location_based_attention.py
+++ location_based_attention.py
@@ -27,7 +27,7 @@ location_based_attention = nn.Linear(encoder_hidden_size*2, sequence_length)
 
 trainable_parameters = [{'params': net.parameters()} for net in [embedding, encoder, decoder, decoder_output_to_sequence_length, location_based_attention]]
 
-optimizer = Adam(trainable_parameters, lr=0.005)
+optimizer = Adam(trainable_parameters, lr=0.001)
 
 embedding.to(device)
 encoder.to(device)
@@ -35,7 +35,7 @@ decoder.to(device)
 decoder_output_to_sequence_length.to(device)
 location_based_attention.to(device)
 
-decoder_input = torch.zeros(1, sequence_length, decoder_input_size)
+decoder_input = torch.zeros(1, 1, decoder_input_size)
 
 def train():
     correct, total = 0, 0
@@ -47,12 +47,17 @@ def train():
         random_sequence_embedding = embedding(random_sequence)
         encoder_outputs, (encoder_h, encoder_c) = encoder(random_sequence_embedding)
 
-        # attention starts here
-        encoder_output_weights = F.softmax(location_based_attention(encoder_h.view(1,1,-1)), dim=2)
-        weighted_sum_of_encoder_outputs = torch.bmm(encoder_output_weights, encoder_outputs)
-        # attention ends here
-
-        decoder_outputs, (decoder_h, decoder_c) = decoder(decoder_input, (encoder_h.view(1,1,-1), weighted_sum_of_encoder_outputs))
+        decoder_outputs = []
+        decoder_input_h = encoder_h.view(1,1,-1)
+        for t in range(sequence_length):
+            # attention starts here
+            encoder_output_weights = F.softmax(location_based_attention(decoder_input_h), dim=2)
+            weighted_sum_of_encoder_outputs = torch.bmm(encoder_output_weights, encoder_outputs)
+            # attention ends here
+            decoder_output_at_time_t, (decoder_h, decoder_c) = decoder(decoder_input, (decoder_input_h, weighted_sum_of_encoder_outputs))
+            decoder_outputs.append(decoder_output_at_time_t)
+            decoder_input_h = decoder_h
+        decoder_outputs = torch.cat(decoder_outputs, 1)
         softmax_input = decoder_output_to_sequence_length(decoder_outputs).squeeze(0)
 
         loss = criterion(softmax_input, correct_sequence)
